bc1bced6359bdca2811ed5cddde296c77f3030f5
Cristian Marcucci <cristian.marcucci@gmail.com>
2022-07-05 22:00:47 -0300

    long way
    JavaScript almost done, starting CSS setup.

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ let uploadedImg = document.createElement('img');
 let imgWidth;
 let imgHeight;
 
-function imgSize(image){
+//Functio that determines the size of the image displayed
+/*function imgSize(image){
     if(image.width<500){
         imgWidth = image.width;
     } else {
@@ -27,21 +28,31 @@ function imgSize(image){
     }
     imgHeight = image.height * Number(imgWidth/image.width);
     console.log(imgWidth, imgHeight);
+}*/
+
+function imgDisplayFunction(){
+        //Display image properly
+        /*imgSize(uploadedImg);
+        imgHolder.width=imgWidth;
+        imgHolder.height=imgHeight;*/
+        ctx.drawImage(uploadedImg, 0, 0, 500, 500);
 }
 
 //Upload the image file
 imgUploadInput.addEventListener('change', function(){
     let reader = new FileReader();
+
     reader.addEventListener('load', ()=> {
+        if(reader.result){
         uploadedImg.src = reader.result;
         imgHolder.appendChild(uploadedImg);
         imgDisplay.appendChild(imgHolder);
+        console.log(uploadedImg);
+        imgDisplayFunction()
 
-        //Display image properly
-        imgSize(uploadedImg);
-        imgHolder.width=imgWidth;
-        imgHolder.height=imgHeight;
-        ctx.drawImage(uploadedImg, 0, 0, imgWidth, imgHeight);
+        } else{
+            window.alert("Load unsuccessful! Try again.")
+        }
     })
     reader.readAsDataURL(this.files[0]);
 })
@@ -59,8 +70,8 @@ function Filter(){
     let saturationValue = saturationSlider.value;
     let sepiaValue = sepiaSlider.value;
     ctx.filter = `brightness(${brightnessValue}%) contrast(${contrastValue}%) saturate(${saturationValue}%) sepia(${sepiaValue})`;
-    imgSize(uploadedImg);
-    ctx.drawImage(uploadedImg, 0, 0, imgWidth, imgHeight);
+    //imgSize(uploadedImg);
+    ctx.drawImage(uploadedImg, 0, 0, 500, 500);
 }
 
 //Brightness ajustment
@@ -78,3 +89,53 @@ saturationSlider.addEventListener('input', Filter);
 //Sepia ajustment
 let sepiaSlider = document.querySelector('#sepiaSlider');
 sepiaSlider.addEventListener('input', Filter);
+
+//Download modified image
+let downloadButton = document.querySelector('.downloadButton');
+
+function downloadImg(){
+    
+    if(imgHolder.hasChildNodes()){//Checks if there is an image uploaded
+        if(window.navigator.msSaveBlob){//Method for IE
+            window.navigator.msSaveBlob(imgHolder.msBlob(), "sof-filter.jpg");
+        } else{
+            const a  = document.createElement('a');//Method for Mozila and Chrome
+            a.href = imgHolder.toDataURL();
+            a.download = 'soft-filter.jpg';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    }else{
+        window.alert("No picture uploaded")
+    }        
+}
+
+downloadButton.addEventListener('click', downloadImg)
+
+//Reset the modified image
+let resetButton = document.querySelector('.resetButton');
+
+function resetImg(){
+    
+    if(imgHolder.hasChildNodes()){//Checks if there is an image uploaded
+        ctx.filter = `brightness(100%) contrast(100%) saturate(100%) sepia(0)`;
+        //imgSize(uploadedImg);
+        ctx.drawImage(uploadedImg, 0, 0, 500, 500);
+
+        let resetSlide = document.querySelectorAll(".slider")
+        resetSlide.forEach(function(item){
+               if(item.classList.contains('sepia')){
+                    item.value=0;
+               } else if(item.classList.contains('contrast')) {
+                item.value=200;
+               } else{
+                item.value=100;
+               }
+        })
+    }else{
+        window.alert("No picture uploaded")
+    }        
+}
+
+resetButton.addEventListener('click', resetImg)
